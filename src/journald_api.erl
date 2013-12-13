19695660a0c11271cc9cf199fb6952261cce73c5
Mirjam Friesen <mf@travelping.com>
2013-12-13 13:49:48 +0100

    [ejournald] Add support for pids in sendv
    Summary: sendv accepts pids now
    
    Test Plan: sendv([{"SYSLOG_PID", self()}])
    
    Reviewers: hsychla, skattelmann, orixmann, hwinkel
    
    Reviewed By: skattelmann
    
    CC: druss, aschultz
    
    Differential Revision: https://review.tpip.net/D1025

--- src/journald_api.erl
+++ src/journald_api.erl
@@ -120,6 +120,7 @@ list_conversion(_,_) -> [].
 to_list(V) when is_integer(V) -> integer_to_list(V);
 to_list(V) when is_float(V)   -> float_to_list(V);
 to_list(V) when is_atom(V)    -> atom_to_binary(V, utf8);
+to_list(V) when is_pid(V)     -> pid_to_list(V);
 to_list(V) -> V. 
 
 load_nif() ->
