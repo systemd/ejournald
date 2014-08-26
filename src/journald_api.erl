5d3521ea0bb0c2bb1cab4851e3ee9575a6448427
Sascha Kattelmann <skattelmann@gmail.com>
2014-08-26 18:06:48 +0200

    forgot the erlang api

--- src/journald_api.erl
+++ src/journald_api.erl
@@ -44,128 +44,38 @@ nif_stub_error(Line) ->
 %% ----------------------------------------------------------------------------------
 %% -- low level journald API
 sendv(Args) ->
-    error_wrapper( fun sendv_nif/1, [list_conversion(Args)] ).
+    sendv_nif(list_conversion(Args)).
 
-stream_fd(Id, Prio, LvlPrefix) -> 
-    error_wrapper( fun stream_fd_nif/3, [Id, Prio, LvlPrefix] ).
-
-write_fd(Fd, Msg) ->
-    error_wrapper( fun write_fd_nif/2, [Fd, Msg] ).
-
-close_fd(Fd) ->
-    error_wrapper( fun close_fd_nif/1, [Fd] ).
-
-open() ->
-    error_wrapper( fun open_nif/0, [] ).
-
-open_directory(Dir) ->
-    error_wrapper( fun open_directory_nif/1, [Dir] ).
-
-close(Fd) ->    
-    error_wrapper( fun close_nif/1, [Fd] ).
-
-next(Fd) ->
-    error_wrapper( fun next_nif/1, [Fd] ).
-
-previous(Fd) -> 
-    error_wrapper( fun previous_nif/1, [Fd] ).
-
-get_data(Fd, Field) -> 
-    error_wrapper( fun get_data_nif/2, [Fd, Field] ).
-
-add_match(Fd, Field) -> 
-    error_wrapper( fun add_match_nif/2, [Fd, Field] ).
-
-add_disjunction(Fd) -> 
-    error_wrapper( fun add_disjunction_nif/1, [Fd] ).
-
-flush_matches(Fd) -> 
-    error_wrapper( fun flush_matches_nif/1, [Fd] ).
-
-seek_head(Fd) -> 
-    error_wrapper( fun seek_head_nif/1, [Fd] ).
-
-seek_tail(Fd) -> 
-    error_wrapper( fun seek_tail_nif/1, [Fd] ).
-
-get_cursor(Fd) ->
-    error_wrapper( fun get_cursor_nif/1, [Fd] ).
-
-test_cursor(Fd, Cursor) ->
-    error_wrapper( fun test_cursor_nif/2, [Fd, Cursor] ).
-
-seek_cursor(Fd, Cursor) ->
-    error_wrapper( fun seek_cursor_nif/2, [Fd, Cursor] ).
-
-query_unique(Fd, Field) ->
-    error_wrapper( fun query_unique_nif/2, [Fd, Field] ).
-
-enumerate_unique(Fd) ->
-    error_wrapper( fun enumerate_unique_nif/1, [Fd] ).
-
-restart_unique(Fd) ->
-    error_wrapper( fun restart_unique_nif/1, [Fd] ).
-
-open_notifier(Fd, Pid) ->
-    error_wrapper( fun open_notifier_nif/2, [Fd, Pid] ).
-
-enumerate_data(Fd) ->
-    error_wrapper( fun enumerate_data_nif/1, [Fd] ).
-
-restart_data(Fd) ->
-    error_wrapper( fun restart_data_nif/1, [Fd] ).
-
-close_notifier(Fd) ->
-    error_wrapper( fun close_notifier_nif/1, [Fd] ).
-
-get_realtime_usec(Fd) ->
-    error_wrapper( fun get_realtime_usec_nif/1, [Fd] ).
-
-seek_realtime_usec(Fd, Usec) ->
-    error_wrapper( fun seek_realtime_usec_nif/2, [Fd, Usec] ).
-
-
-%% ----------------------------------------------------
-%% --  NIF dummies
 sendv_nif(_Args) -> ?nif_stub.
-stream_fd_nif(_A, _B, _C) -> ?nif_stub.
-write_fd_nif(_Fd, _Msg) -> ?nif_stub.
-close_fd_nif(_Fd) -> ?nif_stub.
-open_nif() -> ?nif_stub.
-open_directory_nif(_Arg) -> ?nif_stub.
-close_nif(_Arg) -> ?nif_stub.    
-next_nif(_Arg) -> ?nif_stub.
-previous_nif(_Arg) -> ?nif_stub. 
-get_data_nif(_Arg1, _Arg2) -> ?nif_stub. 
-add_match_nif(_Arg1, _Arg2) -> ?nif_stub. 
-add_disjunction_nif(_Arg) -> ?nif_stub. 
-flush_matches_nif(_Arg) -> ?nif_stub. 
-seek_head_nif(_Arg) -> ?nif_stub. 
-seek_tail_nif(_Arg) -> ?nif_stub. 
-get_cursor_nif(_Arg) -> ?nif_stub.
-test_cursor_nif(_Arg1, _Arg2) -> ?nif_stub.
-seek_cursor_nif(_Arg1, _Arg2) -> ?nif_stub.
-query_unique_nif(_Arg1, _Arg2) -> ?nif_stub.
-enumerate_unique_nif(_Arg) -> ?nif_stub.
-restart_unique_nif(_Arg) -> ?nif_stub.
-open_notifier_nif(_Arg1, _Arg2) -> ?nif_stub.
-enumerate_data_nif(_Arg) -> ?nif_stub.
-restart_data_nif(_Arg) -> ?nif_stub.
-close_notifier_nif(_Arg) -> ?nif_stub.
-get_realtime_usec_nif(_Journal) -> ?nif_stub.
-seek_realtime_usec_nif(_Journal, _Usec) -> ?nif_stub.
-
+stream_fd(_A, _B, _C) -> ?nif_stub.
+write_fd(_Fd, _Msg) -> ?nif_stub.
+close_fd(_Fd) -> ?nif_stub.
+open() -> ?nif_stub.
+open_directory(_Arg) -> ?nif_stub.
+close(_Arg) -> ?nif_stub.    
+next(_Arg) -> ?nif_stub.
+previous(_Arg) -> ?nif_stub. 
+get_data(_Arg1, _Arg2) -> ?nif_stub. 
+add_match(_Arg1, _Arg2) -> ?nif_stub. 
+add_disjunction(_Arg) -> ?nif_stub. 
+flush_matches(_Arg) -> ?nif_stub. 
+seek_head(_Arg) -> ?nif_stub. 
+seek_tail(_Arg) -> ?nif_stub. 
+get_cursor(_Arg) -> ?nif_stub.
+test_cursor(_Arg1, _Arg2) -> ?nif_stub.
+seek_cursor(_Arg1, _Arg2) -> ?nif_stub.
+query_unique(_Arg1, _Arg2) -> ?nif_stub.
+enumerate_unique(_Arg) -> ?nif_stub.
+restart_unique(_Arg) -> ?nif_stub.
+open_notifier(_Arg1, _Arg2) -> ?nif_stub.
+enumerate_data(_Arg) -> ?nif_stub.
+restart_data(_Arg) -> ?nif_stub.
+close_notifier(_Arg) -> ?nif_stub.
+get_realtime_usec(_Journal) -> ?nif_stub.
+seek_realtime_usec(_Journal, _Usec) -> ?nif_stub.
 
 %% -----------------------------------------------------------------
 %% -- helpers
-error_wrapper(Fun, Args) ->
-    case erlang:apply(Fun, Args) of
-        badarg ->
-            erlang:error(badarg, Args);
-        Result ->
-            Result
-    end.
- 
 list_conversion([])    -> [];
 list_conversion([{E,V}|T])  ->
     [[E, $=, to_list(V)] | list_conversion(T)]; 
